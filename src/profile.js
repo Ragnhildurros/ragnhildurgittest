ed7666e3652e0e71e87c1271e5749b3835f0d6c0
Ragnhildur Rós <ragnhildurros@gmail.com>
2019-03-14 10:48:44 +0000

    bjó til profilesíðu

---
+++ src/profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component{
+
+    render(){
+        return(
+            <div>this is a profile page</div>
+        )
+    }
+}
+
+
+export default Profile;
